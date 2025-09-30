6498669d5331fa2c420454ad6e5c5843acf7682d
David Pilar <dpilar@vendavo.com>
2025-09-30 08:48:47 +0200

    unit test

--- src/test/java/com/vendavo/eps/todos/service/UserServiceTest.java
+++ src/test/java/com/vendavo/eps/todos/service/UserServiceTest.java
@@ -16,18 +16,25 @@ public class UserServiceTest {
     private UserService service;
     private IExternalUserClient client;
 
+    private List<User> users;
+
     @BeforeEach
     public void before() {
         client = mock(IExternalUserClient.class);
         service = new UserService(client);
+
+        User user1 = mock(User.class);
+        when(user1.getId()).thenReturn(1L);
+        User user2 = mock(User.class);
+        when(user2.getId()).thenReturn(2L);
+        User user3 = mock(User.class);
+        when(user3.getId()).thenReturn(3L);
+        users = List.of(user1, user2, user3);
+        when(client.getUsers()).thenReturn(users);
     }
 
     @Test
     public void testGetUsers() {
-        // given
-        List<User> users = List.of(mock(User.class), mock(User.class), mock(User.class));
-        when(client.getUsers()).thenReturn(users);
-
         // when
         List<User> result = service.getUsers();
 
@@ -37,47 +44,46 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testGetUser() {
-        // given
-        User user1 = mock(User.class);
-        when(user1.getId()).thenReturn(1L);
-        User user2 = mock(User.class);
-        when(user2.getId()).thenReturn(2L);
-        User user3 = mock(User.class);
-        when(user3.getId()).thenReturn(3L);
-        List<User> users = List.of(user1, user2, user3);
-        when(client.getUsers()).thenReturn(users);
-
+    public void testGetUser1() {
         // when
         User result = service.getUser(1L);
 
         // then
         assertNotNull(result);
         assertEquals(1L, result.getId());
-        verify(client, times(1)).getUsers();
+        verify(client).getUsers();
+    }
 
+    @Test
+    public void testGetUser2() {
         // when
-        result = service.getUser(2L);
+        User result = service.getUser(2L);
 
         // then
         assertNotNull(result);
         assertEquals(1L, result.getId());
-        verify(client, times(2)).getUsers();
+        verify(client).getUsers();
+    }
 
+    @Test
+    public void testGetUser3() {
         // when
-        result = service.getUser(3L);
+        User result = service.getUser(3L);
 
         // then
         assertNotNull(result);
         assertEquals(1L, result.getId());
-        verify(client, times(3)).getUsers();
+        verify(client).getUsers();
+    }
 
+    @Test
+    public void testGetUser4() {
         // when
-        result = service.getUser(4L);
+        User result = service.getUser(4L);
 
         // then
         assertNotNull(result);
         assertEquals(1L, result.getId());
-        verify(client, times(4)).getUsers();
+        verify(client).getUsers();
     }
 }
